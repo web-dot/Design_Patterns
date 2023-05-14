1e5b9e7aa47f2a7d52f4160a192faed1bbf19898
sudipto <cg.sudipc@gmail.com>
2023-05-14 10:40:04 +0530

    Decorator pattern - simple test

---
+++ src/com/design/decorator_pattern/TestDecorator.java
@@ -0,0 +1,12 @@
+package com.design.decorator_pattern;
+
+public class TestDecorator {
+	
+	public static void main(String[] args) {
+		// caramel `Decorated` espresso
+		Beverage b = new Caramel(new Espresso());
+		int cost = b.cost();
+		System.out.println("cost of caramel decorated espresso is : " + cost);
+	}
+	
+}
