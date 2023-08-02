32a2c03fff8107d990ba3aae83ab09e1fb37654b
sudipto <cg.sudipc@gmail.com>
2023-08-02 23:41:31 +0530

    strategy pattern simple diagram

--- src/com/design/strategy_pattern_test/TestStrategy.java
+++ src/com/design/strategy_pattern_test/TestStrategy.java
@@ -1,7 +1,26 @@
 package com.design.strategy_pattern_test;
 
+
+/**
+ * 						Interface{behavior}	(InterfaceEnroll)	
+ * 							/|\						  /\
+ * 							 |i							\i
+ * 			 				Class(SchoolStudentEnroll)   Class(CollegeStudentEnroll)
+ * 							/|\							   /|\
+ *			uses			 |								|
+ *Client ---------->	 	instance1						|		
+ *(SchoolStudent)											|
+ * 															|
+ *Client 			 uses behavior from instance2			|					
+ * (CollegeStudent)---------------------------------->    instance2
+ * 
+ * 
+ * */
+
+
+
+
 public class TestStrategy {
-	
 	
 	public static void main(String[] args) {		
 		CollegeStudentEnroll collegeStudentEnroll = new CollegeStudentEnroll();
