821977f9821075647c00ad2e3849386b5b5d47d9
sudipto <cg.sudipc@gmail.com>
2023-11-07 21:59:49 +0530

    pizza-shop : factory method pattern

---
+++ src/com/design/factory_method_pattern_pizza_shop/PizzaShop.java
@@ -0,0 +1,15 @@
+package com.design.factory_method_pattern_pizza_shop;
+
+public class PizzaShop {
+	/**
+	 * Use Case: Pizza ordering system
+	 * 
+	 * imagine you are developing a pizza ordering system for a pizzeria. In this system, customers
+	 * can order different types of pizzas, such as Margherita, Pepperoni, and Veggie. The goal is to
+	 * create a flexible system that allows the addition of new pizza types without modifying
+	 * the existing codebase. This is where the Factory Method Pattern comes in.
+	 * */
+	public static void main(String[] args) {
+		
+	}
+}
